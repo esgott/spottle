61557886181df428a54550a98a07415d336c2ef3
Zsolt Szilagyi <esgott@gmail.com>
2021-10-09 10:03:11 +0200

    Add ingress

--- scripts/create-local-cluster.sh
+++ scripts/create-local-cluster.sh
@@ -9,10 +9,30 @@ kind create cluster \
     --config "${SCRIPT_DIR}/kind-config.yaml" \
     --wait 5m
 
-kubectl --context kind-spottle create namespace spottle
 kubectl --context kind-spottle create namespace kafka
+kubectl --context kind-spottle create namespace spottle
 
-kubectl --context kind-spottle --namespace kafka apply -f "${SCRIPT_DIR}/kafka-deployment.yaml"
+kubectl apply \
+    --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+kubectl apply \
+    --context kind-spottle \
+    --namespace kafka \
+    --filename "${SCRIPT_DIR}/kafka-deployment.yaml"
+
+kubectl wait \
+    --timeout=300s \
+    --namespace ingress-nginx \
+    --selector=app.kubernetes.io/component=controller \
+    --for=condition=ready \
+    pod
+
+kubectl wait \
+    --timeout=180s \
+    --namespace kafka \
+    --selector=app=kafka \
+    --for=condition=ready \
+    pod
 
 kafka-topics --bootstrap-server localhost:9092 --create --topic spottle.commands.v1
 kafka-topics --bootstrap-server localhost:9092 --create --topic spottle.events.v1
